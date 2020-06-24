21c0bb0c7e2b6529cb4952c1b2bfc2b072ed0472
Rhafael Santos <rhafa99ss@gmail.com>
2020-06-24 20:43:31 -0300

    A trying to use styled component

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,6 +1,19 @@
 import React from 'react'
 import { Text } from 'react-native'
+import { Wrapper, Header, Balance, BalanceContainer, BalanceTitle } from './styles'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 
-export default function Home(){
-    return <Text>Home</Text>
+ export default function Home(){
+    return (
+        <Wrapper>
+            <Header>
+            <MaterialCommunityIcons name="qrcode-scan" size={30} color="#10c68e" />
+            <BalanceContainer>
+                <BalanceTitle>Meu Saldo</BalanceTitle>
+                <Balance>R$ 0,00</Balance>
+            </BalanceContainer>
+
+            </Header>
+        </Wrapper>
+    )
 }
