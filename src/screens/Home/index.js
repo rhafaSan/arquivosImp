b7b9e685cb1bcffdbd7593c933ff97e8cf97c4be
Rhafael Santos <rhafa99ss@gmail.com>
2020-06-26 20:31:18 -0300

    Suggestion's screen done

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,11 +1,18 @@
 import React from 'react'
-import { Text } from 'react-native'
-import { Wrapper, Header, Balance, BalanceContainer, BalanceTitle } from './styles'
-import { MaterialCommunityIcons } from '@expo/vector-icons'
+
+import { Wrapper, Header,Container, Balance, BalanceContainer, BalanceTitle } from './styles'
+
+import { MaterialCommunityIcons, AntDesign } from '@expo/vector-icons'
+
+import Suggestions from '../../components/Suggestions'
+import Activities from '../../components/Activities'
+
 
  export default function Home(){
     return (
         <Wrapper>
+            <Container>
+
             <Header>
             <MaterialCommunityIcons name="qrcode-scan" size={30} color="#10c68e" />
             <BalanceContainer>
@@ -13,7 +20,15 @@ import { MaterialCommunityIcons } from '@expo/vector-icons'
                 <Balance>R$ 0,00</Balance>
             </BalanceContainer>
 
+            <AntDesign name="gift" size={30} color="#10c68e" />
+
             </Header>
+
+            <Suggestions />
+
+            <Activities />
+
+            </Container>
         </Wrapper>
     )
 }
